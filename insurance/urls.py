cc4c97fc8d5ddb6c41de9f619a9ed4e9ecd6ab6c
Denmark Garcia <dengarcia.x@gmail.com>
2026-03-13 23:32:38 +0800

    added lang ng hola

--- insurance/urls.py
+++ insurance/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import InsuranceDetail, InsuranceList, InsuranceCreateFakeData
+from .views import InsuranceDetail, InsuranceList, InsuranceCreateFakeData, InsuranceTest
 
 urlpatterns = [
     path("", InsuranceList.as_view()),
     path("<int:pk>/", InsuranceDetail.as_view()),
     # Test data
     path("test/", InsuranceCreateFakeData.as_view()),
+    path("hola/", InsuranceTest.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
